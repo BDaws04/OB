743fb46e9de1c42cc7b320baaf32d11be0f3986d
BDaws04 <ben.dawson@kcl.ac.uk>
2025-05-10 16:21:24 +0100

    add order.h and .cpp

--- include/order.h
+++ include/order.h
@@ -5,10 +5,20 @@
 
 struct alignas(16) Order {
     public:
+        bool is_buy() const;
+        bool is_GTC() const;
+        bool is_IOC() const;
+        bool is_FOK() const;
         uint32_t id;
         uint32_t price;
         uint32_t volume;
-        uint32_t metadata; 
+        Order(uint32_t id, uint32_t price, uint32_t volume, bool is_buy, bool is_GTC, bool is_IOC, bool is_FOK)
+            : id(id), price(price), volume(volume) {
+                encode_metadata(is_buy, is_GTC, is_IOC, is_FOK); 
+         }
+    private:
+        uint32_t metadata;
+        void encode_metadata(bool is_buy, bool is_GTC, bool is_IOC, bool is_FOK);
 };
 
 #endif
