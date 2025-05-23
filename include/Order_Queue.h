8c85715b12d35926af59d73b9a1291cdc1e61cde
BDaws04 <ben.dawson@kcl.ac.uk>
2025-05-23 16:16:57 +0100

    add v1 order queue. simple spinlock wrapped around a queue

--- include/Order_Queue.h
+++ include/Order_Queue.h
@@ -1,16 +1,72 @@
 #ifndef ORDER_QUEUE_H
 #define ORDER_QUEUE_H
 
+#include <queue>
+#include <cstdint>
 #include <atomic>
+#include <mutex>
+#include <optional>
 
 template <typename T>
 class OrderQueue {
-    public:
-        ...
     private:
-        // power of 2 buffer size for fast modulo operation
-        // this is a circular buffer, so the size must be a power of 2
-        const size_t _bufer_size = 1024; 
+        std::queue<T> queue_;
+        SpinLock lock_;
+
+    public:
+        OrderQueue() = default;
+
+        void push(const T& order) {
+            lock_.lock();
+            queue_.push(order);
+            lock_.unlock();
+        }
+
+        T pop() {
+            lock_.lock();
+            if (!queue_.empty()) {
+                T order = queue_.front();
+                queue_.pop();
+                lock_.unlock();
+                return order;
+            }
+            lock_.unlock();
+        }
+
+        bool empty() {
+            lock_.lock();
+            bool result = queue_.empty();
+            lock_.unlock();
+            return result;
+        }
+
+        size_t size() {
+            lock_.lock();
+            size_t result = queue_.size();
+            lock_.unlock();
+            return result;
+        }
+};
+
+class SpinLock {
+public:
+    SpinLock() : flag(ATOMIC_FLAG_INIT) {}
+
+    void lock() {
+        while (flag.test_and_set(std::memory_order_acquire)) {
+        }
+    }
+
+    void unlock() {
+        flag.clear(std::memory_order_release);
+    }
+
+    bool try_lock() {
+        return !flag.test_and_set(std::memory_order_acquire);
+    }
+
+private:
+    std::atomic_flag flag;
 };
 
 
--- src/Order_Queue.tpp
+++
@@ -1,2 +0,0 @@
-#include "../include/Order_Queue.h"
-
